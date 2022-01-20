460ad485fbe690b5c9572390a2365ac117a3a9d5
Arian Mohajer <amohajer45@gmail.com>
2022-01-20 15:54:33 -0700

    basic layout for detail view

--- BookStore/BookStore/Controller/View Controller/BookDetailViewController.swift
+++ BookStore/BookStore/Controller/View Controller/BookDetailViewController.swift
@@ -9,21 +9,23 @@ import UIKit
 
 class BookDetailViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var authorTextField: UITextField!
+    @IBOutlet weak var ratingTextField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var bookImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveButtonTapped(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func clearButtonTapped(_ sender: Any) {
+    }
 }
